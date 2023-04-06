798cb4f35f3319cf33221209b21a08936bb53fb0
Jayasurya <jsuryagowri@gmail.com>
2023-04-06 09:11:43 +0530

    changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,15 +10,46 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   slides = [
-    {img: "https://dummyimage.com/350x150/423b42/fff"},
-    {img: "https://dummyimage.com/350x150/2a2b7a/fff"},
-    {img: "https://dummyimage.com/350x150/1a2b7a/fff"},
-    {img: "https://dummyimage.com/350x150/7a2b7a/fff"},
-    {img: "https://dummyimage.com/350x150/9a2b7a/fff"},
-    {img: "https://dummyimage.com/350x150/5a2b7a/fff"},
-    {img: "https://dummyimage.com/350x150/4a2b7a/fff"}
+    {img: "../../assets/image/banner-01.png"},
+    {img: "../../assets/image/banner-02.jpg"},
+    {img: "../../assets/image/banner-03.jpg"},
+    {img: "../../assets/image/banner-04.jpg"},
+    {img: "../../assets/image/mehandigif.gif"}
   ];
-  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
+  slideConfig = {
+    "slidesToShow": 4,
+    "slidesToScroll": 1,
+    "dots": true,
+    "infinite": true,
+    "autoplay" : true,
+    "autoplaySpeed" : 1000,
+    "arrows":false,
+    "responsive": [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
    
   slickInit(e:any) {
     console.log('slick initialized');
